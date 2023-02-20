ffda5e7e2979cd20b8542dbfe203c8c14a01f4ce
Kandy Krakon <114084368+EcoliEater87@users.noreply.github.com>
2023-02-20 01:41:22 +0000

    Initial commit

---
+++ web/eaglercraft_opts.js
@@ -0,0 +1,10 @@
+window.eaglercraftXOpts = () => ({
+	lang: "en_US",
+  // joinServer: "ws" + window.location.href.slice(4),
+  // assetsURI: "custom.epk",
+	servers: [
+		{ addr: "ws" + window.location.href.slice(4), name: "Franklin Is Dead and We Have Killed Him" }
+	]
+});
+// document.title = "Franklin Is Dead and We Have Killed Him:tm:";
+// document.querySelector("link[rel=\"shortcut icon\"]").href = "https://a.pinatafarm.com/400x400/25e85b97cf/quandale-dingle.jpg";
